b14871521eee0fb5ccc908ce6d04f9a2c30de82c
Adi <daimagine@gmail.com>
2013-04-02 16:47:16 +0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NuAyogrosir::Application.config.secret_token = '875abe43e8d62a05b747e6ee638c4209f8290f0585e4e0e77a505daf472edb76c346457979f725b085fa83e6d25573289d60634dd2ecadad81724ccd1dcfae32'
