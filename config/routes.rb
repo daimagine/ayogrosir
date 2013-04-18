d556735bdc00380a3680c4e851395a49fd91e1c9
adi kurniawan <daimagine@gmail.com>
2013-04-18 12:17:17 +0700

    initialize admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 NuAyogrosir::Application.routes.draw do
   
+  get "dashboard/index"
+
   devise_for :users
   resources :users
   
@@ -18,9 +20,17 @@ NuAyogrosir::Application.routes.draw do
     end
   end
 
+  # products routes
   resources :products, :only => [:index, :show]
 
+  # manage static pages
   get '/:slug' => 'pages#index', as: 'page'
+
+  # manage admin (backend)
+  match :admin, :to => 'admin/dashboards#index'
+  namespace :admin do
+    resources :users, :markets, :stores, :products
+  end
 
   authenticated :user do
     root :to => 'home#index'
